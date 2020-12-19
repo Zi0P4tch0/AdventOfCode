7e477e4d86ca128c445ecf5c64a42525d34bbedb
Matteo Pacini <m+github@matteopacini.me>
2020-12-19 06:16:21 +0000

    Day 17 stub

---
+++ aoc2020_day17.c
@@ -0,0 +1,17 @@
+#include <glib.h>
+
+void aoc2020_day17_format_data(const gchar **lines, guint n_lines) {
+
+}
+
+void aoc2020_day17_p1(guint n_run) {
+
+}
+
+void aoc2020_day17_p2(guint n_run) {
+
+}
+
+void aoc2020_day17_cleanup() {
+
+}
